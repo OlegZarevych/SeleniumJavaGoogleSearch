ca10ceca3aef97980bbf82ddbe0285ad11d09ad0
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-02-26 01:42:45 +0200

    update dependency

--- src/main/java/ozar/SeleniumJavaGoogleSearch/BaseTest/BaseTest.java
+++ src/main/java/ozar/SeleniumJavaGoogleSearch/BaseTest/BaseTest.java
@@ -1,5 +1,14 @@
 package ozar.SeleniumJavaGoogleSearch.BaseTest;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -16,10 +25,18 @@ public class BaseTest {
 		browser = factory.getBrowser();
 		browser.startDriver();
 	}
-	
+
+	@AfterMethod
+	public void takeScreenShotOnFailure(ITestResult testResult) throws IOException {
+		if (testResult.getStatus() == ITestResult.FAILURE) {
+			File scrFile = ((TakesScreenshot) browser).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, new File("errorScreenshots\\" + testResult.getName() + "-"
+					+ Arrays.toString(testResult.getParameters()) + ".jpg"));
+		}
+	}
+
 	@AfterTest
-	void closeBrowser()
-	{
+	void closeBrowser() {
 		browser.stopDriver();
 	}
 }
